fea8891b04de758435d4b9a98a9bbac23d35781c
V.VISWESHKISHAN <v.viswesh04@gmail.com>
2024-06-30 08:50:14 +0530

    db upgrade

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 });
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://luciferbrine04:KIS04han@lucy.hdza0s9.mongodb.net/blog", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB Atlas'))
     .catch(err => console.error('Failed to connect to MongoDB Atlas', err));
 
