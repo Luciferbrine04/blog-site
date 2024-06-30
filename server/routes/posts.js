a1620909b379e9ff773f792874d76e4b27516374
V.VISWESHKISHAN <v.viswesh04@gmail.com>
2024-06-30 20:29:04 +0530

    reupload old code

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,66 +2,62 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-module.exports = (io) => {
-    // Get all posts
-    router.get('/', async (req, res) => {
-        try {
-            const posts = await Post.find();
-            res.json(posts);
-        } catch (err) {
-            res.json({ message: err });
-        }
+// Get all posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// Get a specific post
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// Create a post
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
     });
 
-    // Get a specific post
-    router.get('/:postId', async (req, res) => {
-        try {
-            const post = await Post.findById(req.params.postId);
-            res.json(post);
-        } catch (err) {
-            res.json({ message: err });
-        }
-    });
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
 
-    // Create a post
-    router.post('/', async (req, res) => {
-        const post = new Post({
-            title: req.body.title,
-            content: req.body.content
-        });
+// Delete a post
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postId });
+        res.json(removedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
 
-        try {
-            const savedPost = await post.save();
-            io.emit('postCreated', savedPost); // Emit event
-            res.json(savedPost);
-        } catch (err) {
-            res.json({ message: err });
-        }
-    });
+// Update a post
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            { _id: req.params.postId },
+            { $set: { title: req.body.title, content: req.body.content } }
+        );
+        res.json(updatedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
 
-    // Delete a post
-    router.delete('/:postId', async (req, res) => {
-        try {
-            const removedPost = await Post.findByIdAndRemove(req.params.postId);
-            io.emit('postDeleted', removedPost._id); // Emit event
-            res.json(removedPost);
-        } catch (err) {
-            res.json({ message: err });
-        }
-    });
-
-    // Update a post
-    router.patch('/:postId', async (req, res) => {
-        try {
-            const updatedPost = await Post.updateOne(
-                { _id: req.params.postId },
-                { $set: { title: req.body.title, content: req.body.content } }
-            );
-            res.json(updatedPost);
-        } catch (err) {
-            res.json({ message: err });
-        }
-    });
-
-    return router;
-};
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,18 @@
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const http = require('http');
-const { Server } = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
-});
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // Import Routes
-const postsRoute = require('./routes/posts')(io); // Pass io to the routes
+const postsRoute = require('./routes/posts');
 app.use('/posts', postsRoute);
 
 // Routes
@@ -35,6 +26,7 @@ mongoose.connect("mongodb+srv://luciferbrine04:KIS04han@lucy.hdza0s9.mongodb.net
     .catch(err => console.error('Failed to connect to MongoDB Atlas', err));
 
 // Start the server
-server.listen(process.env.PORT || 5000, () => {
-    console.log(`Server running on port ${process.env.PORT || 5000}`);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server running on port ${port}`);
 });
